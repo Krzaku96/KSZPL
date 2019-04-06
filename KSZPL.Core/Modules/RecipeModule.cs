ee246754c53289a2eefc9312cd7e8911d64e9163
Robert <robert.krzaczynski@gmail.com>
2019-04-06 18:02:48 +0200

    Add recipe CRUD - backend

---
+++ KSZPL.Core/Modules/RecipeModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using KSZPL.Data.Models;
+using KSZPL.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KSZPL.Core.Modules
+{
+    class RecipeModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Repository<Recipe>>().As<IRepository<Recipe>>().InstancePerDependency();
+        }
+    }
+}
