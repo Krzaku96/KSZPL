8edde8845d5a5cd7cef60428cd5c14bf2e94cd6f
Bartosz Pawelec <226462@student.pwr.edu.pl>
2019-04-14 18:59:02 +0200

    Show users

---
+++ KSZPL.Frontend/kszpl.frontend/src/components/ShowUsersComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Row, Col } from "react-bootstrap";
+
+class ShowUsersComponent extends Component {
+  render() {
+    return (
+      <Col>
+        <Row>Users</Row>
+      </Col>
+    );
+  }
+}
+
+export default ShowUsersComponent;
