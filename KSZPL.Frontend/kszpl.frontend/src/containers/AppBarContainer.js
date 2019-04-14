f9a44d25f67dc88a91490daf24c40257bf132dc9
Bartosz Pawelec <226462@student.pwr.edu.pl>
2019-04-14 18:58:11 +0200

    Add check role and show users functions

--- KSZPL.Frontend/kszpl.frontend/src/containers/AppBarContainer.js
+++ KSZPL.Frontend/kszpl.frontend/src/containers/AppBarContainer.js
@@ -1,21 +1,70 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import AppBarComponent from "../components/AppBarComponent";
+import axios from "axios";
+import { BASE_URL } from "../constants";
 
 class AppBarContainer extends Component {
   state = {
-    username: ""
+    username: "",
+    role: "",
+    users: []
   };
+
+  componentWillMount = () => {
+    this.checkRole();
+  };
+
   redirectToHome = () => {
     this.context.router.history.push("/");
   };
 
   redirectToDetails = () => {
-    this.getDataFromLocalStorage();
     return this.props.history.push({
       pathname: "/details"
     });
   };
+
+  redirectToShowUsers = () => {
+    return this.props.history.push({
+      pathname: "/showUsers",
+      state: {
+        users: this.state.users
+      }
+    });
+  };
+
+  checkRole = () => {
+    var role = JSON.parse(localStorage.getItem("role"));
+    this.setState({ role: role });
+  };
+
+  showUsersOnClick = event => {
+    event.preventDefault();
+
+    let axiosConfig = {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
+    };
+
+    axios
+      .get(BASE_URL + "Users", axiosConfig)
+      .then(response => {
+        if (response.data) {
+          this.setState({ users: response.data});
+          this.redirectToShowUsers();
+          console.log(response);
+        } else {
+          console.log("Can't find response");
+        }
+      });
+  };
+
+  addUserOnClick = () => {
+    //to do
+  }
 
   render() {
     return (
@@ -23,6 +72,8 @@ class AppBarContainer extends Component {
         redirectToHome={this.redirectToHome}
         redirectToDetails={this.redirectToDetails}
         user={this.props.user}
+        role={this.state.role}
+        showUsersOnClick={this.showUsersOnClick}
       />
     );
   }
