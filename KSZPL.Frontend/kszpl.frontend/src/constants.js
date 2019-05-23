b02ec76d3e3dbcc45a1af8f4de62c23a96b26843
Bartosz Pawelec <226462@student.pwr.edu.pl>
2019-05-23 23:18:19 +0200

    React Big Calendar + fix create visit + add styles

--- KSZPL.Frontend/kszpl.frontend/src/constants.js
+++ KSZPL.Frontend/kszpl.frontend/src/constants.js
@@ -1,2 +1,2 @@
-//export const BASE_URL = "https://localhost:44367/"
-export const BASE_URL = "https://localhost:5001/"
+export const BASE_URL = "https://localhost:44367/"
+//export const BASE_URL = "https://localhost:5001/"
